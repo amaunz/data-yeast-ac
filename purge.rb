9574e884046145ff162ad5adcab1a103369ce1aa
Andreas Maunz <andreas@maunz.de>
2008-12-08 09:53:35 +0100

    Removed superfluous activities

---
+++ purge.rb
@@ -0,0 +1,27 @@
+
+def GetKeys(keys);
+    File.open(ARGV[1].to_s, "r") {|file|
+        oldkey = ""
+        file.readlines.each{|line|
+            key = line.split[0]
+            if (key.size && key != oldkey)
+                keys << key
+            end
+            oldkey = key
+        }
+    }
+    keys
+end
+
+keys = []
+keys = GetKeys(keys)
+File.open(ARGV[0].to_s, "r") { |file|
+    lines_arr = file.readlines
+    i=0
+    lines_arr.each { |line|
+        hay = line.split[0]
+        if !keys.include?(hay) then
+            puts line
+        end
+    }
+}
